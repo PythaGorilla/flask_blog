02933ce7485e00de4ec36fdd46ba769d3f54d863
James Moore <jammoore8931@gmail.com>
2015-02-23 16:48:31 -0500

    config chinese word

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-import os
 # -*- coding: utf-8 -*-
 # ...
+
+import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
